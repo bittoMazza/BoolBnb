f4cab0f6d6e49a2381a8bf6784398dfb7421f4e3
bittoMazza <robertomazza99@gmail.com>
2022-10-11 12:15:46 +0200

    feat:create users and apartments table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('email')->unique();
+            $table->string('name');
+            $table->string('surname');
+            $table->date('date_birth');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2022_10_11_100749_create_apartments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('rooms');
+            $table->integer('beds');
+            $table->integer('bathrooms');
+            $table->integer('square_meters');
+            $table->string('address');
+            $table->text('image');
+            $table->boolean('is_visible');
+            $table->decimal('long', 10, 7);
+            $table->decimal('lat', 10, 7);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
