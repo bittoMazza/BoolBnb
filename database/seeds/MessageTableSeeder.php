3eb35fbf2eec6868143ff135728452e65d79e6fd
vincaf <vincenzocafassog@gmail.com>
2022-10-11 17:26:21 +0200

    feat(MessageTableSeeder): add apartment_id

--- database/seeds/MessageTableSeeder.php
+++ database/seeds/MessageTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Apartment;
 use App\Models\Message;
 use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
@@ -13,9 +14,11 @@ class MessageTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $apartments = Apartment::all();
 
         for ($i = 0; $i < 10; $i++) {
             $message = new Message();
+            $message->apartment_id = $faker->randomElement($apartments)->id;
             $message->name = $faker->firstName();
             $message->surname = $faker->lastName();
             $message->email = $faker->email();
