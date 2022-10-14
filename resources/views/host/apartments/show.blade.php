6410b42cda558b6b08398d2258daee1675587094
vincaf <vincenzocafassog@gmail.com>
2022-10-14 16:52:15 +0200

    feat(show): add gallery images

--- resources/views/host/apartments/show.blade.php
+++ resources/views/host/apartments/show.blade.php
@@ -13,28 +13,27 @@
                         <h1>{{ session('edited') }}</h1>
                     </div>
                 @endif
-                <div class="card">
-                    {{-- @forelse ($apartment->messages as $message)
-                        <div class="my-2">
-                            <div><span class="font-weight-bold">Autore:</span> {{ $message->name }}</div>
-                            <div><span class="font-weight-bold">Mail:</span> {{ $message->email }}</div>
-                            <p><span class="font-weight-bold">Contenuto:</span> {{ $message->content }}</p>
+                <div class="card my-4">
+                    <div class="row">
+                        <div class="row row-cols-3">
+                            @forelse ($apartment->images as $image)
+                                <div class="col">
+                                    @if (filter_var($image->image, FILTER_VALIDATE_URL))
+                                        <img src="{{ $image->image }}" alt="{{ $apartment->title }}"
+                                            class="w-100 shadow-1-strong rounded mb-4" />
+                                        {{-- url --}}
+                                    @else
+                                        <img src="{{ asset('storage/' . $image->image) }}"
+                                            alt="{{ $apartment->title }}" class="w-100 shadow-1-strong rounded mb-4" />
+                                        {{-- file --}}
+                                    @endif
+                                </div>
+                                @empty
+                                    Non ci sono immagini
+                                @endforelse
                         </div>
-                    @empty
-                        Non ci sono messaggi
-                    @endforelse --}}
-                    <div class="d-flex flex-wrap">
-                        @forelse ($apartment->images as $image)
-                            @if (filter_var($image->image, FILTER_VALIDATE_URL))
-                                <img src="{{ $image->image }}" alt="{{ $apartment->title }}" class="w-50 rounded-start" />
-                                {{-- url --}}
-                            @else
-                                <img src="{{ asset('storage/' . $image->image) }}" alt="{{ $apartment->title }}" class="img-fluid rounded-start" />
-                                {{-- file --}}
-                            @endif   
-                        @empty
-                            Non ci sono immagini
-                        @endforelse
+
+
                     </div>
 
                     <div class="card-body">
@@ -43,7 +42,8 @@
                         <p class="card-text">Bagni: {{ $apartment->bathrooms }}</p>
                         <p class="card-text">Letti: {{ $apartment->beds }}</p>
                         <p class="card-text">Indirizzo: {{ $apartment->address }}</p>
-                        <form action="{{ route('host.apartments.destroy', $apartment->id) }}" method="POST" class="d-inline             form-apartment-delete">
+                        <form action="{{ route('host.apartments.destroy', $apartment->id) }}" method="POST"
+                            class="d-inline             form-apartment-delete">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="btn btn-warning">
