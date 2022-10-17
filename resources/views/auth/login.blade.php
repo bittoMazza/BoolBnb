4cc0645d05e3737f742677ebfc8836e21416b325
EvoX7 <creativesoultouch@gmail.com>
2022-10-17 10:31:03 +0200

    fix: fixed login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,9 +51,9 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group row mb-0 mt-3">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary text-white fw-bold">
+                                <button type="submit" class="btn btn-primary text-white">
                                     {{ __('Login') }}
                                 </button>
 
