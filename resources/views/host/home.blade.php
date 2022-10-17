9d17abc2bf24513972fd84a2c5e88b4ad2068729
EvoX7 <creativesoultouch@gmail.com>
2022-10-17 10:26:04 +0200

    fix: loader fixed plus minor fixes to login

--- resources/views/host/home.blade.php
+++ resources/views/host/home.blade.php
@@ -1,12 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="text-center">
+    <div class="text-center mt-5">
         <h1>Benvenuto</h1>
         <h2>{{ Auth::user()->name }}</h2>
     </div>
 
-    
-
-
+    <div class="loader">
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square last"></div>
+        <div class="square clear"></div>
+        <div class="square"></div>
+        <div class="square last"></div>
+        <div class="square clear"></div>
+        <div class="square "></div>
+        <div class="square last"></div>
+    </div>
 @endsection
