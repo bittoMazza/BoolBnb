c77f1e3425d5a3faf9cc54328af7821e9a0ad518
vincaf <vincenzocafassog@gmail.com>
2022-10-18 16:22:41 +0200

    refactor(HeaderWeb): edit style on header

--- resources/js/components/HeaderWeb.vue
+++ resources/js/components/HeaderWeb.vue
@@ -1,22 +1,24 @@
 <template>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="/">BoolBnB</a>
+                <a class="p-0 me-4" href="/">
+                    <img class="header_img" src="https://tinypic.host/images/2022/10/14/Logo_sm.png" alt="BoolBnB">
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto w-100 align-items-center">
-                        <li class="nav-item active">
+                    <ul class="navbar-nav mr-auto w-100 align-items-center align-items-middle">
+                        <li class="nav-item active fw-bold">
                             <router-link class="nav-link" to="/">
                                 Home
                             </router-link>
                         </li>
 
-                        <li class="nav-item ml-auto">
-                            <a href="/host" class="ms_link badge badge-light">
+                        <li class="nav-item ms-auto fw-bold">
+                            <a href="/host" class="nav-link">
                                 Login
                             </a>
                         </li>
@@ -34,7 +36,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.ms_link{
-    font-size: 14px;
-}
+
 </style>
