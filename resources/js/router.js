8e8a5b7e040600c1546320c59e5bb47d11d35073
vincaf <vincenzocafassog@gmail.com>
2022-10-18 15:59:29 +0200

    refactor(router.js): remove example components

--- resources/js/router.js
+++ resources/js/router.js
@@ -4,8 +4,6 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter)
 
 import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactsPage from './pages/ContactsPage';
 
 const router = new VueRouter({
     mode: 'history',
