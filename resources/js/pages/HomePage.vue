9b650c8fab9aaffbe43fcf77b2da11ca24cd924a
vincaf <vincenzocafassog@gmail.com>
2022-10-18 15:57:24 +0200

    feat(HomePage): create home

--- resources/js/pages/HomePage.vue
+++ resources/js/pages/HomePage.vue
@@ -1,15 +1,20 @@
 <template>
-  <section>
-    <MainComponent />
-  </section>
+  <main class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>MAIN</h1>
+        </div>
+    </div>
+
+  </main>
 </template>
 
 <script>
-import MainComponent from "../components/Main.vue";
+
 export default {
   name: "HomePage",
   components: {
-    MainComponent,
+
   },
 };
 </script>
