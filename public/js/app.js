16355fef2d87905c440d590c4d288749093dbbb8
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-10-19 10:38:53 +0200

    feat: check if asterisk is needed

--- public/js/app.js
+++ public/js/app.js
@@ -41482,8 +41482,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\vince\Desktop\Boolean\Esercizi\BoolBnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\vince\Desktop\Boolean\Esercizi\BoolBnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\haksn\Desktop\BoolBnb\BoolBnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\haksn\Desktop\BoolBnb\BoolBnb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
