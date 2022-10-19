bd3a2efb6d903efa02fc3a2acb4623e0484fcaec
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-10-19 10:37:13 +0200

    feat: asterisk in register blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row py-2">
-                            <label for="Nome" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+                            <label for="Nome" class="col-md-4 col-form-label text-md-right">{{ __('Nome*') }}</label>
 
                             <div class="col-md-6">
                                 <input id="Nome" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group row py-2">
-                            <label for="Cognome" class="col-md-4 col-form-label text-md-right">{{ __('Cognome') }}</label>
+                            <label for="Cognome" class="col-md-4 col-form-label text-md-right">{{ __('Cognome*') }}</label>
 
                             <div class="col-md-6">
                                 <input id="Cognome" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
@@ -40,7 +40,7 @@
                         </div>
 
                         <div class="form-group row py-2">
-                            <label for="Data di nascita" class="col-md-4 col-form-label text-md-right">{{ __('Data di nascita') }}</label>
+                            <label for="Data di nascita" class="col-md-4 col-form-label text-md-right">{{ __('Data di nascita*') }}</label>
 
                             <div class="col-md-6">
                                 <input id="Data di nascita" type="date" class="form-control @error('date_birth') is-invalid @enderror" name="date_birth" value="{{ old('date_birth') }}" required autocomplete="date_birth" autofocus>
@@ -54,7 +54,7 @@
                         </div>
 
                         <div class="form-group row py-2">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email*') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -68,7 +68,7 @@
                         </div>
 
                         <div class="form-group row py-2">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password*') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -82,7 +82,7 @@
                         </div>
 
                         <div class="form-group row py-2">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password*') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
