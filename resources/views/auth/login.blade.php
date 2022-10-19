b3cc9eabb2e30451f1acb11d79e746d458e2a1c4
EvoX7 <creativesoultouch@gmail.com>
2022-10-19 12:32:57 +0200

    fix: fixed login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,11 +28,11 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
+                            <div class="form-group row mt-3">
                                 <label for="password"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                                <div class="col-md-6 mt-3">
+                                <div class="col-md-6">
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
                                         required autocomplete="current-password">
