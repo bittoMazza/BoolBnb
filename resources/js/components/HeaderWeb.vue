c18f948835f0d1249f89e73aedfbab206a3a38bf
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-10-25 17:45:45 +0200

    refactor: css footer and footer position

--- resources/js/components/HeaderWeb.vue
+++ resources/js/components/HeaderWeb.vue
@@ -3,43 +3,20 @@
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
       <div class="container">
         <a class="p-0 me-4" href="/">
-          <img
-            class="header_img ms-2"
-            src="https://tinypic.host/images/2022/10/14/Logo_sm.png"
-            alt="BoolBnB"
-            
-          />
+          <img class="header_img ms-2" src="https://tinypic.host/images/2022/10/14/Logo_sm.png" alt="BoolBnB"/>
         </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul
-            class="navbar-nav w-100 d-flex justify-content-between align-middle"
-          >
-            <div class="d-flex">
-              <li class="nav-item active fw-bold mt-2">
-                <router-link class="nav-link" to="/"> Home </router-link>
-              </li>
-            </div>
-
-            <div class="d-flex mt-2">
-              <li class="nav-item fw-bold">
-                <a href="/host" class="nav-link"> LOGIN </a>
-              </li>
-              <li class="nav-item fw-bold">
-                <a href="/register" class="nav-link"> REGISTRATI </a>
-              </li>
-            </div>
+          <ul class="navbar-nav w-100 d-flex justify-content-end align-middle mt-2">
+            <li class="nav-item fw-bold">
+              <a href="/host" class="nav-link"> LOGIN </a>
+            </li>
+            <li class="nav-item fw-bold">
+              <a href="/register" class="nav-link"> REGISTRATI </a>
+            </li>
           </ul>
         </div>
       </div>
