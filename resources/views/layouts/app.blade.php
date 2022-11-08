3d4dbc6dfb6dbda5f7da29c825c355ee1b1c91e9
EvoX7 <creativesoultouch@gmail.com>
2022-11-08 11:06:50 +0100

    feat: map with markers added to show blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,17 +11,14 @@
     <title>BoolBnb</title>
 
     <!-- Scripts -->
-    <link rel='stylesheet' type='text/css' href='https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.20.0/maps/maps.css'>
 
-<script type='text/javascript' src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.20.0/maps/maps-web.min.js"></script>
 
-<script type='text/javascript'
-    src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.1.2-public-preview.15/services/services-web.min.js"></script>
-    <script src="{{ asset('js/map.js') }}" defer></script>
+
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/register_form_validation.js') }}" defer></script>
     <script src="{{ asset('js/tomtom_coordinates.js') }}" defer></script>
     <script src="{{ asset('js/apartment_form_validation.js') }}" defer></script>
+
     
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
         integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
@@ -35,6 +32,7 @@
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
 
+   
 
 
 
@@ -46,9 +44,17 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    {{-- MapStyleCss --}}
+    <link rel='stylesheet' type='text/css' href='https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.20.0/maps/maps.css'>
+
+    {{-- MapScript --}}
+    <script type='text/javascript' src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.20.0/maps/maps-web.min.js">
+    </script>
 </head>
 
 <body>
@@ -59,6 +65,146 @@
         </main>
         @include('partials.footer')
     </div>
+
+    <script>
+        const API_KEY = '6qNAEpN1aWvsSHDysFdG1qoHsAhaUIQ0';
+
+
+        let map = tt.map({
+
+            key: API_KEY,
+
+            container: 'map-div',
+
+            center: [14.0, 42.0],
+
+            zoom: 5.5
+
+        });
+
+        // Apartment locator 
+
+        const apartments = {
+            "type": "FeatureCollection",
+            "features": [{
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [
+                            14.031702205442677,
+                            40.76199237334906
+                        ]
+                    },
+                    "properties": {
+                        "title": "Casa sul mare",
+                        "address": "Via Salita Castello 13, 80079 Procida, Italia",
+                        "city": "Procida NA"
+                    }
+                },
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [
+                            14.019365200587943,
+                            40.7577703070356
+                        ]
+                    },
+                    "properties": {
+                        "title": "Corallo Residence",
+                        "address": "Via Vittorio Emanuele 284, 80079 Procida, Italia",
+                        "city": "Procida NA"
+                    }
+                },
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [
+                            14.530960987775696,
+                            36.781475462893944
+                        ]
+                    },
+                    "properties": {
+                        "title": "IzzHome Bella Vista",
+                        "address": "Via Perugia 67, 97100 Marina di Ragusa, Italia",
+                        "city": "Ragusa SR"
+                    }
+                },
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [
+                            12.383846649136192,
+                            44.21331410599949
+                        ]
+                    },
+                    "properties": {
+                        "title": "Villetta al mare",
+                        "address": "Viale Giuseppe Mazzini 182, 47042 Cesenatico, Emilia Romagna",
+                        "city": "Cesenatico FC"
+                    }
+                },
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [
+                            12.554644359866689,
+                            44.047578422255526
+                        ]
+                    },
+                    "properties": {
+                        "title": "Appartamento vicino a Rimini",
+                        "address": "Via Cristoforo Fontemaggi, 4, 47923 Rimini RN",
+                        "city": "Rimini RN"
+                    }
+                }
+            ]
+        };
+
+        const markersCity = [];
+
+        apartments.features.forEach(function(apartment, index) {
+            const title = apartment.properties.title;
+            const city = apartment.properties.city;
+            const address = apartment.properties.address;
+            const location = apartment.geometry.coordinates;
+
+
+            let markStyle = document.createElement('div');
+            markStyle.id = 'marker';
+
+            const marker = new tt.Marker({
+                element: markStyle
+            }).setLngLat(location).addTo(map);
+
+
+            let popupOffset = {
+                top: [0, 0],
+                bottom: [0, -70],
+                'bottom-right': [0, -70],
+                'bottom-left': [0, -70],
+                left: [25, -35],
+                right: [-25, -35]
+            };
+
+            let popup = new tt.Popup({
+                offset: popupOffset
+            }).setHTML(`<h2> ${title} </h2><h4> ${address} </h4>`);
+
+            marker.setPopup(popup);
+
+            markersCity[index] = {
+                marker,
+                city
+            };
+        });
+    </script>
+
+
+    {{-- <script src="{{ asset('js/map.js') }}"></script> --}}
 </body>
 
 </html>
